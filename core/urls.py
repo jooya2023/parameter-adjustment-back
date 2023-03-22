8ea7ac56cb8a659745c081ac343ce6fa0d4da836
vahidzare <vahid.zare.ce@gmail.com>
2023-03-22 16:03:11 +0330

    add url parameter and update swagger

--- core/urls.py
+++ core/urls.py
@@ -18,22 +18,25 @@ from django.urls import path, include, re_path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Parameters setting Api",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=[],
+    openapi.Info(
+        title="Parameters setting Api",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[],
+    authentication_classes=[JWTAuthentication]
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("accounts.api.urls")),
+    path("", include("parameter.api.urls")),
     re_path(r'^api/docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
