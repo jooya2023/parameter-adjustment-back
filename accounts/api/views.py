0ac8122842cd79383c85323d683c0629d86ce118
vahidzare <vahid.zare.ce@gmail.com>
2023-03-22 16:02:05 +0330

    add refresh token in redis

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -3,13 +3,23 @@ from rest_framework.response import Response
 
 from accounts.api.serializers import LoginSerializer, RegisterSerializer
 
+from core.helper.redis import Redis
+
 
 class LoginGenericView(generics.GenericAPIView):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.redis = Redis()
+
     serializer_class = LoginSerializer
 
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data, context={"request": request})
         serializer.is_valid(raise_exception=True)
+        username = serializer.data.get("username")
+        refresh_token = serializer.data["tokens"]["refresh"]
+        self.redis.redis_add_refresh_token(username, refresh_token)
         return Response(serializer.data, status=200)
 
 
